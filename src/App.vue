e300567c12462a317ad58770c4879d2890314084
Blackn <nilinfeng6318@gmail.com>
2021-10-17 00:07:37 +0800

    chore:引入 vant 以及按需引入插件

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <van-button type="primary">主要按钮</van-button>
+  <van-icon name="chat-o" />
 </template>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import { Button,Icon } from "vant";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(Button).use(Icon);
+app.mount("#app");
