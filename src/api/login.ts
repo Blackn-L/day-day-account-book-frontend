dab1fffe1ffc9cd0dcd288cad4b112ba87b19871
Blackn <nilinfeng6318@gmail.com>
2021-10-23 20:18:20 +0800

    wip:增加登录接口

--- src/api/login.ts
+++ src/api/login.ts
@@ -3,10 +3,23 @@ interface LoginParams {
   username: string;
   password: string;
 }
+interface RegisterParams {
+  username: string;
+  password: string;
+}
 // 登录
 export function login(params: LoginParams) {
   return request({
     url: "/user/login",
+    method: "post",
+    data: params,
+  });
+}
+
+// 注册
+export function register(params: RegisterParams) {
+  return request({
+    url: "/user/register",
     method: "post",
     data: params,
   });
