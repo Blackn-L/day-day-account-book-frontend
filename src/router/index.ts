a39c7337a110eb7afc239a8cac441c67ad17d7e4
Blackn <nilinfeng6318@gmail.com>
2021-11-11 21:46:14 +0800

    feat:设置全局守卫,判断是否有token

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,18 +3,26 @@ const routes = [
   {
     path: "/",
     component: () => import("@/views/bill/index.vue"),
+    meta: { checkAuth: true },
   },
   {
     path: "/count",
     component: () => import("@/views/count/index.vue"),
+    meta: { checkAuth: true },
   },
   {
     path: "/user",
     component: () => import("@/views/user/index.vue"),
+    meta: { checkAuth: true },
   },
   {
     path: "/login",
     component: () => import("@/views/login/index.vue"),
+  },
+  {
+    path: "/detail",
+    component: () => import("@/views/bill/components/BillDetail.vue"),
+    meta: { checkAuth: true },
   },
 ];
 
@@ -22,6 +30,23 @@ const router = createRouter({
   // 使用 hash 模式。
   history: createWebHashHistory(),
   routes,
+});
+router.beforeEach((to, from, next) => {
+  if (to.meta.checkAuth) {
+    // 是否需要验证 token
+    if (localStorage.getItem("token")) {
+      // 是否存在 token
+      next();
+    } else {
+      next({
+        path: "/login", // 将跳转的路由path作为参数，登录成功后跳转到该路由
+        query: { redirect: to.fullPath },
+      });
+    }
+  } else {
+    // 不需要 token，继续跳转
+    next();
+  }
 });
 
 export default router;
