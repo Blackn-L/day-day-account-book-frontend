a2098b8c3edc92eabeaceed2ae5a7a3a2ce1dc05
Blackn <nilinfeng6318@gmail.com>
2021-11-22 20:34:01 +0800

    wip:暂时提交

--- src/views/count/index.vue
+++ src/views/count/index.vue
@@ -1,9 +1,59 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, computed } from "vue";
+import * as dayjs from "dayjs";
+import PopDate, { API as PopDateAPI } from "@/components/PopDate.vue";
+const refPopDate = ref<PopDateAPI | undefined>();
+const selectedDate = ref(new Date());
+const totalExpenses = ref(0); // 总支出
+const totalIncome = ref(0); // 总收入
+// 账单时间改变
+const handelChangeDate = (date: Date) => {
+  if (refPopDate.value) refPopDate.value.showDate = false;
+  selectedDate.value = date;
+};
+
 </script>
 
 <template>
-  <van-button>count</van-button>
+  <div>
+    <div class="header">
+      <div>
+        <span
+          class="date"
+          @click="
+            () => {
+              if (refPopDate) refPopDate.showDate = true;
+            }
+          "
+        >
+          {{ dayjs(selectedDate).format("YYYY-MM")
+          }}<van-icon name="arrow-down"
+        /></span>
+      </div>
+      <span>共支出</span>
+      <span>￥{{ totalExpenses }}</span>
+      <span>共收入 ￥{{ totalIncome }}</span>
+    </div>
+  </div>
+  <!-- 账单时间弹窗 -->
+  <PopDate
+    ref="refPopDate"
+    :selected-date="selectedDate"
+    @on-change="handelChangeDate"
+  />
 </template>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.header {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  margin: 30px 20px;
+  .date {
+    background-color: #f5f5f5;
+    font-size: 16px;
+    padding: 7px;
+  }
+}
+</style>
