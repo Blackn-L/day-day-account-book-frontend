933c7738864f13019c4667ea7197cc0a7ad947ef
Blackn <nilinfeng6318@gmail.com>
2021-12-03 10:15:42 +0800

    feat:新增更新密码接口

--- src/api/user.ts
+++ src/api/user.ts
@@ -6,6 +6,18 @@ export interface LoginAndRegParams {
 interface LoginData {
   token: string;
 }
+interface editParams {
+  signature: string;
+}
+interface editData {
+  id: number;
+  username: string;
+  signature: string;
+}
+interface updatePasswordParams {
+  oldPassword: string;
+  newPassword: string;
+}
 // 登录
 export function login(params: LoginAndRegParams) {
   return request<LoginData>({
@@ -25,9 +37,27 @@ export function register(params: LoginAndRegParams) {
 }
 
 // 获取用户信息
-export function getUserInfo(params: LoginAndRegParams) {
+export function getUserInfo() {
   return request({
     url: "/user/get",
     method: "get",
+  });
+}
+
+// 编辑用户信息
+export function editUserInfo(params: editParams) {
+  return request<editData>({
+    url: "/user/edit",
+    method: "put",
+    data: params,
+  });
+}
+
+// 修改密码
+export function updatePassword(params: updatePasswordParams) {
+  return request({
+    url: "/user/update_password",
+    method: "put",
+    data: params,
   });
 }
